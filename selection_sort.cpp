f87b339f9a634ba9bffaa4cf774ad3122b7f04ad
Madan R <24BEIS117@gcu.edu.in>
2026-03-24 20:50:55 +0530

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void insertionSort(int arr[], int n) {
-    for (int i = 1; i < n; i++) {
+void insertionSort(int arr[], int n) 
+{
+    for (int i = 1; i < n; i++) 
+    {
         int key = arr[i];
         int j = i - 1;
 
         // Shift elements greater than key
-        while (j >= 0 && arr[j] > key) {
+        while (j >= 0 && arr[j] > key) 
+        {
             arr[j + 1] = arr[j];
             j--;
         }
@@ -17,14 +20,16 @@ void insertionSort(int arr[], int n) {
     }
 }
 
-int main() {
+int main() 
+{
     int arr[] = {12, 11, 13, 5, 6};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     insertionSort(arr, n);
 
     cout << "Sorted array: ";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         cout << arr[i] << " ";
     }
 
